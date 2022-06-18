29e4d24fb81e76c08b330ed979ad45dcc90283e4
BorisSavateev <tonars1327@mail.ru>
2022-06-19 01:40:03 +1000

    Доработат файл подсчета оценок classroom.js

---
+++ task 29.05.22/project. Подсчет оценок/js/classroom.js
@@ -0,0 +1,30 @@
+/* classroom.js */
+
+/** @param {number[]} grades */
+/** @param {number[]} grades */
+export const getNumberOfGrades = grades => grades.length;
+  
+  /** @param {number[]} grades */
+  export const getSumGrades = grades => {
+    // let sum = 0;
+    // grades.forEach(grade => {sum += grade});
+    // return sum; 
+    return grades.reduce((total, current) =>  total + current);
+  }
+  
+  /** @param {number[]} grades */
+  export const getAverageValue = grades => getSumGrades(grades) / getNumberOfGrades(grades);
+  
+  /** @param {number[]} grades */
+  export const getPassingGrades = grades => {
+    return grades.filter(grade => grade >= 10);
+  }
+  
+  /** @param {number[]} grades */
+  export const getFailingGrades = grades => {
+    return grades.filter(grade => grade < 9);
+  }
+  
+  /** @param {number[]} grades */
+  export const getRaisedGrades = grades => grades.map(grade => (grade === 20) ? 20 : grade + 1);
+  
